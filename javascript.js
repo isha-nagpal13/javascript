d0ff4e1a42172e80e8ff6f2d1b6b2708476f957f
Isha Nagpal <ishanagpalofficial@gmail.com>
2024-07-19 23:39:13 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,38 @@
+/*//que 1 
+let name="isha nagpal";
+console.log(name);
+
+//que 2 didnt understand
+let gender="female";
+let female="true";
+
+//que 3 */ /*
+
+let answer=0;
+for(let i=0;i<=1000;i++) {
+answer= answer +1;
+}
+/*
+
+//que 4 */ /*
+
+let numbers=[39,28,56,88];
+for( let i=0;i<numbers.length;i++) {
+if (numbers[i] % 2==0) {
+    console.log(numbers[i] ,"are even");
+}
+}
+*/
+
+//que 5
+
+let arr = [1, 88, 39, 748, 940, 834];
+let max = arr[0];
+
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+        max = arr[i];
+    }
+}
+
+console.log("The greatest number is:", max);
